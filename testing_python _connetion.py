f136412631615818b7943902e285efa580aede69
e-perry24 <elizabeth.perry24@bathspa.ac.uk>
2025-05-09 10:38:34 +0100

    testing python flask method

--- testing_python _connetion.py
+++ testing_python _connetion.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template, request
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
 
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def form():
-    return render_template('form.html')
+# The route() function of the Flask class is a decorator, 
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'Hello World'
 
-@app.route('/hello', methods=['GET', 'POST'])
-def hello():
-    return render_template('greeting.html', say=request.form['say'], to=request.form['to'])
+# main driver function
+if __name__ == '__main__':
 
-if __name__ == "__main__":
+    # run() method of Flask class runs the application 
+    # on the local development server.
     app.run()
-
-    # might need to downloade a "PIP" libery on python for this method to work(looking into it anaconda )
